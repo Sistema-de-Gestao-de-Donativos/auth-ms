1aee9f0731fd97e9a9cb9a7d2c2f6a20e152197f
Ramiro Nilson Barros <ramiro.nilson.barros@gmail.com>
2024-12-05 14:13:12 -0300

    Add Cors middleware

--- auth_ms/__init__.py
+++ auth_ms/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import api, dependencies
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title="Users Microservice", version="0.0.1")
+    app = FastAPI(title="Auth Microservice", version="0.0.1")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allows all origins
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all methods
+        allow_headers=["*"],  # Allows all headers
+    )
     dependencies.init_app(app)
     api.init_app(app)
     return app
